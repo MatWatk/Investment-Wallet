0968b461d5eeaafca88d1003fb69abeb97e2f5e4
MatWatk <mateusz.watkowski@gmail.com>
2026-06-05 15:06:11 +0200

    Swapped logo with asset name

--- src/components/AssetTable/AssetPositionName.tsx
+++ src/components/AssetTable/AssetPositionName.tsx
@@ -1,8 +1,8 @@
 export default function AssetTablePosition({name, image}: {name: string, image: string}) {
     return (
         <div className="min-w-25 flex-1 flex items-center gap-2 whitespace-nowrap">
-            <p className="font-medium whitespace-nowrap">{name}</p>
             <img src={image} alt={`${name} logo`} className="h-6 w-6 ml-3 shrink-0" />
+            <p className="font-medium whitespace-nowrap">{name}</p>
         </div>
     )
 }
