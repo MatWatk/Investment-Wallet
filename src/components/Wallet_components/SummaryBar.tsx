78d9efe488dcf89b402735be7dca769f68855383
MatWatk <mateusz.watkowski@gmail.com>
2026-06-15 18:52:34 +0200

    Removed comment.

--- src/components/Wallet_components/SummaryBar.tsx
+++ src/components/Wallet_components/SummaryBar.tsx
@@ -9,7 +9,6 @@ export default function SummaryBar({ totalValue }: { totalValue: number }) {
 
     return (
         <div className={`flex flex-row items-center gap-2 p-4 rounded-md bg-linear-to-b ${themeState ? "from-gray-200 to-violet-300" : "from-gray-700 to-gray-900"} h-10 w-full shrink-0`}>
-            {/* <h1 className="text-xl font-bold ml-4">Summary</h1> */}
             <div className="ml-auto flex flex-row gap-2 shrink-0 items-center whitespace-nowrap">
                 <p className="w-25 text-center flex items-center justify-center gap-2 shrink-0 mr-8">{translations[language].walletPage.summaryBar}</p>
                 <p className="font-bold mr-4">{totalValue.toFixed(2)}</p>
