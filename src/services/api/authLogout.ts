82c1d3bcf70eb3afbb82c35077b92c43398c6329
MatWatk <mateusz.watkowski@gmail.com>
2026-07-07 22:08:17 +0200

    Added redux user cleaning on logout.

--- src/services/api/authLogout.ts
+++ src/services/api/authLogout.ts
@@ -1,9 +1,12 @@
 import { auth } from '../../services/firebase/config';
 import { signOut } from 'firebase/auth';
+import { authActions } from '../../store/authSlice';
+import { store } from '../../store';
 
 export default async function logout() {
     try {
         await signOut(auth);
+        store.dispatch(authActions.setAuth({ isAuthenticated: false, user: null }));
     } catch (error) {
         throw error;
     }
