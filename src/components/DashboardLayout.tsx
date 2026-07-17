5e48dac0dba22e51c78051bdc9ac5448e6ad637d
MatWatk <mateusz.watkowski@gmail.com>
2026-07-17 21:02:49 +0200

    Fixed redirection after login token expired

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,27 +1,40 @@
 import NavBar from "../components/NavBar_components/NavBar";
 import SideBar from "./SideBar_components/SideBar";
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useNavigation, useNavigate } from "react-router-dom";
 
 import { useTheme } from "../hooks/useTheme";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../services/firebase/config";
+import { useEffect } from "react";
 
 export default function Layout() {
     const navigation = useNavigation();
     const themeState = useTheme();
+    const navigate = useNavigate();
 
-    return (
-        <div className={`flex h-screen w-full flex-col ${themeState ? "bg-linear-to-t from-violet-200 to-white" : "bg-linear-to-t from-gray-700 to-gray-900 text-yellow-500"}`}>
-            <NavBar />
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                navigate("/login");
+            }
+            return () => unsubscribe();
+        }
+        )}, [navigate]);
 
-            <div className="flex flex-1 overflow-hidden">
-                <SideBar />
+        return (
+            <div className={`flex h-screen w-full flex-col ${themeState ? "bg-linear-to-t from-violet-200 to-white" : "bg-linear-to-t from-gray-700 to-gray-900 text-yellow-500"}`}>
+                <NavBar />
 
-                <main className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden p-6">
-                    {navigation.state === "loading" && (
+                <div className="flex flex-1 overflow-hidden">
+                    <SideBar />
+
+                    <main className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden p-6">
+                        {navigation.state === "loading" && (
                             <p className={`${themeState ? "text-violet-400" : "text-yellow-400"} text-2xl flex items-center`}>Loading...</p>
-                    )}
-                    <Outlet />
-                </main>
+                        )}
+                        <Outlet />
+                    </main>
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
