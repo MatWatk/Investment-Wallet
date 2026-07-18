a844aa1e82c213f96c1494617342e54d56cc0543
MatWatk <mateusz.watkowski@gmail.com>
2026-07-18 15:47:41 +0200

    Fixed loading status on lazy loading.

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -19,22 +19,34 @@ export default function Layout() {
             }
             return () => unsubscribe();
         }
-        )}, [navigate]);
+        )
+    }, [navigate]);
 
+    if (navigation.state === "loading") {
         return (
             <div className={`flex h-screen w-full flex-col ${themeState ? "bg-linear-to-t from-violet-200 to-white" : "bg-linear-to-t from-gray-700 to-gray-900 text-yellow-500"}`}>
                 <NavBar />
-
                 <div className="flex flex-1 overflow-hidden">
                     <SideBar />
-
-                    <main className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden p-6">
-                        {navigation.state === "loading" && (
-                            <p className={`${themeState ? "text-violet-400" : "text-yellow-400"} text-2xl flex items-center`}>Loading...</p>
-                        )}
-                        <Outlet />
+                    <main className="flex flex-1 min-w-0 items-center justify-center overflow-y-auto overflow-x-hidden p-6">
+                        <p className={`${themeState ? "text-violet-400" : "text-yellow-400"} text-2xl`}>Loading...</p>
                     </main>
                 </div>
             </div>
         );
     }
+
+    return (
+        <div className={`flex h-screen w-full flex-col ${themeState ? "bg-linear-to-t from-violet-200 to-white" : "bg-linear-to-t from-gray-700 to-gray-900 text-yellow-500"}`}>
+            <NavBar />
+
+            <div className="flex flex-1 overflow-hidden">
+                <SideBar />
+
+                <main className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden p-6">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+}
