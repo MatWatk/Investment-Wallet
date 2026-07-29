77491596869055d6da7c19faf2ca4706847abeae
MatWatk <mateusz.watkowski@gmail.com>
2026-07-29 21:33:58 +0200

    Test config additional fixes.

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,11 +3,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
   testMatch: ['**/tests/**/*.{ts,tsx}', '**/?(*.)+(spec|test).{ts,tsx}'],
-  setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.cjs'],
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json', useESM: true }],
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json' }],
   },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png|jpe?g|webp)$': '<rootDir>/src/test/fileMock.ts',
