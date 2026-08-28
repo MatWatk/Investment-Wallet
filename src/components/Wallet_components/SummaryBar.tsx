1bd79a6b9b616bbc34c8b3c5f0d0b0db4e96787e
MatWatk <mateusz.watkowski@gmail.com>
2026-08-28 15:13:45 +0200

    Summary bar minor styles fix.

--- src/components/Wallet_components/SummaryBar.tsx
+++ src/components/Wallet_components/SummaryBar.tsx
@@ -10,10 +10,10 @@ export default function SummaryBar({ totalValue, textAlign="right" }: { totalVal
 
     return (
         <div className={`flex flex-row items-center gap-2 p-4 rounded-md bg-linear-to-b ${themeState ? "from-gray-200 to-violet-300" : "from-gray-700 to-gray-900"} h-10 w-full shrink-0`}>
-            <div className={`${textAlign === "right" ? "ml-auto justify-end" : "ml-3 justify-start"} flex flex-row gap-2 shrink-0 items-center whitespace-nowrap`}>
-                <p className="w-25 text-center flex items-center justify-center gap-2 shrink-0 mr-8">{translations[language].walletPage.summaryBar}</p>
-                <p className="font-bold mr-4">{totalValue.toFixed(2)}</p>
-                <p className="w-22 text-right shrink-0">{currency}</p>
+            <div className={`${textAlign === "right" ? "ml-auto justify-end" : "ml-3 justify-start"} flex flex-row shrink-0 items-center whitespace-nowrap`}>
+                <p className="w-25 text-center flex items-center justify-center shrink-0 mr-9">{translations[language].walletPage.summaryBar}</p>
+                <p className={`font-bold ${textAlign === "right" ? "mr-4" : "ml-11"}`}>{totalValue.toFixed(2)}</p>
+                <p className={`w-22 text-right shrink-0 ${textAlign === "right" ? "" : "ml-13"}`}>{currency}</p>
             </div>
         </div>
     );
