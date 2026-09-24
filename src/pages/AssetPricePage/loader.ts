4ce7397e016bfc2ab2f11c4bdcad0b4accf918ad
MatWatk <mateusz.watkowski@gmail.com>
2026-09-24 15:35:32 +0200

    Fixed Asset Price Page loader after useExchangeRate hook changes.

--- src/pages/AssetPricePage/loader.ts
+++ src/pages/AssetPricePage/loader.ts
@@ -1,16 +1,16 @@
-// import { assets } from "../../constants/assets";
-// import loadAssetPrices from "../../services/api/loadAssetPrices";
-// import { store } from "../../store/index";
+import { assets } from "../../constants/assets";
+import loadAssetPrices from "../../services/api/loadAssetPrices";
+import { store } from "../../store/index";
 import { checkAuth, getCurrentUser } from "../../utils/utils";
-// import { queryClient } from "../../App";
+import { queryClient } from "../../App";
 
 export async function loader() {
     const loggedUser = await getCurrentUser();
     checkAuth(loggedUser);
-    // const currency = store.getState().currency.currency;
+    const currency = store.getState().currency.currency;
 
-    // await queryClient.ensureQueryData({
-    //     queryKey: ["assetPrices", { assets, currency }],
-    //     queryFn: () => loadAssetPrices<{ coingeckoId: string }[]>({ assets, currency }),
-    // });
+    await queryClient.prefetchQuery({
+        queryKey: ["assetPrices", { assets, currency }],
+        queryFn: () => loadAssetPrices<{ coingeckoId: string }[]>({ assets, currency }),
+    });
 }
